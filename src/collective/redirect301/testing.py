d3c25e50f17159e209fbaaca4c7ba2375a704a91
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-07-19 09:42:14 +0200

    remove robot

--- src/collective/redirect301/testing.py
+++ src/collective/redirect301/testing.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
 from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
@@ -42,14 +41,4 @@ COLLECTIVE_REDIRECT301_INTEGRATION_TESTING = IntegrationTesting(
 COLLECTIVE_REDIRECT301_FUNCTIONAL_TESTING = FunctionalTesting(
     bases=(COLLECTIVE_REDIRECT301_FIXTURE,),
     name="CollectiveRedirect301Layer:FunctionalTesting",
-)
-
-
-COLLECTIVE_REDIRECT301_ACCEPTANCE_TESTING = FunctionalTesting(
-    bases=(
-        COLLECTIVE_REDIRECT301_FIXTURE,
-        REMOTE_LIBRARY_BUNDLE_FIXTURE,
-        z2.ZSERVER_FIXTURE,
-    ),
-    name="CollectiveRedirect301Layer:AcceptanceTesting",
 )
